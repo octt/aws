b2842d31544ba5de2fe8baa087989e3b8053db82
Korben <admin@octt.co>
2016-04-09 15:15:06 -0700

    Ansible setup script

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -7,3 +7,7 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
+
+sudo easy_install pip
+
+pip intall aws
